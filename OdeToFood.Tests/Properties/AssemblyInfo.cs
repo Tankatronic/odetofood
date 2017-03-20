7f418041d0b01b1bbe5a38b9f87fafab47559f70
Justin Loveless <justin.loveless@mccoys.com>
2017-03-20 13:10:36 -0500

    Inital commit #2

--- OdeToFood.Tests/Properties/AssemblyInfo.cs
+++ OdeToFood.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OdeToFood")]
+[assembly: AssemblyTitle("OdeToFood.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("OdeToFood")]
+[assembly: AssemblyProduct("OdeToFood.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("87863815-a708-4c3a-995d-bc0bbc3221e3")]
+[assembly: Guid("d8a82a6e-c0b6-43b6-979b-aa4cda7a2ece")]
 
 // Version information for an assembly consists of the following four values:
 //

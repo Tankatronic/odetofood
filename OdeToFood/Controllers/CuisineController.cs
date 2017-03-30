59f2e72945f60b14922d38fae7b7f515d308afec
Justin Loveless <justin.loveless@mccoys.com>
2017-03-30 16:09:33 -0500

    - Created Index view for Reviews - Created CuisineController

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,16 @@
+﻿using OdeToFood.Filters;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Log]
+    public class CuisineController : Controller
+    {
+        public ActionResult Search(string name = "french")
+        {
+            throw new System.Exception("Something terrible happened");
+            
+            return Content(Server.HtmlEncode(name));
+        }
+    }
+}
